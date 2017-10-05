60fc881898a0fc88cc58a73a15fc9eb33bc56c3e
Kevin Dittman <dittmank@msu.edu>
2017-10-04 22:10:22 -0400

    Working img post with recognition results returned

---
+++ Web_API/TMAAT_WebAPI/Controllers/DBController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TMAAT_WebAPI.Controllers
+{
+    [RoutePrefix("api")]
+    public class DBController : ApiController
+    {
+        [HttpGet]
+        [Route("setItems")]
+        public HttpResponseMessage setItems()
+        {
+            var items = new StringReader(@"Bedroom_KingBed
+Bedroom_QueenFullBed
+Bedroom_DoubleTwinBed
+Bedroom_Crib
+Bedroom_Dresser
+Bedroom_NightStand
+LivingRoom_Couch
+LivingRoom_LoveSeat
+LivingRoom_LargeChair
+LivingRoom_CoffeeTable
+LivingRoom_EndTable
+LivingRoom_EntertainmentCenter
+LivingRoom_Television
+LivingRoom_Lamp
+DiningRoom_DiningTable
+DiningRoom_Chair
+DiningRoom_BuffetTable
+DiningRoom_ChinaCabinet
+Kitchen_MicrowaveToaster
+Kitchen_TeaCart
+Kitchen_Island
+Office_Desk
+Office_Chair
+Office_Computer
+Office_Monitor
+Office_Bookshelf
+Office_FilingCabinet
+Garage_PushPowerMower
+Garage_ToolChest
+Garage_AssortedLawnTools
+Garage_Bicycle
+Patio_Chair
+Patio_SwingGlider
+Patio_Table
+Patio_GrillBarbeque
+SpecialtyItems_ExerciseEquipment
+SpecialtyItems_PoolTable
+SpecialtyItems_LargeSafe
+SpecialtyItems_KitchenAppliances
+SpecialtyItems_LaundryAppliances
+SpecialtyItems_Piano
+SpecialtyItems_HotTub
+Boxes
+BusinessOffice_Desk
+BusinessOffice_Chair
+BusinessOffice_Credenza
+BusinessOffice_FilingCabinet
+BusinessOffice_Bookshelf
+BusinessOffice_Dresser
+BusinessFileRoom_FilingCabinet
+BusinessFileRoom_Bookshelf
+BusinessCopyRoom_SupplyCabinet
+BusinessCopyRoom_ShelvingUnit
+BusinessConferenceRoom_Table
+BusinessConferenceRoom_Chair
+BusinessConferenceRoom_Credenza
+BusinessEquipment_Computer
+BusinessEquipment_Monitor
+BusinessEquipment_CopyMachine
+BusinessEquipment_Printer
+BusinessEquipment_SmallAppliances
+BusinessReceptionArea_Desk
+BusinessReceptionArea_Chair
+BusinessReceptionArea_EndTable");
+            var current = items.ReadLine();
+            using(var sql = new SqlConnection("user id=dittmank;" +
+                                       "password=rdelatable;server=mysql-user.cse.msu.edu;" +
+                                       "Trusted_Connection=false;" +
+                                       "database=dittmank; " +
+                                       "connection timeout=15"))
+            {
+                try
+                {
+                    sql.Open();
+                }
+                catch (Exception e)
+                {
+                    var err = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                    err.ReasonPhrase = e.Message;
+                    return err;
+                }
+                //while (current != null)
+                //{
+                //    current = items.ReadLine();
+                //}
+            }
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ Web_API/TMAAT_WebAPI/Controllers/ImageController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace TMAAT_WebAPI.Controllers
+{
+    [RoutePrefix("api")]
+    public class ImageController : ApiController
+    {
+        public static string WORKING_DIR = "C:\\Users\\Kevin\\Desktop\\darknet-master\\darknet-master\\build\\darknet\\x64";
+        [HttpGet]
+        [Route("test")]
+        public IHttpActionResult Test() {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("uploadImage")]
+        public async Task<HttpResponseMessage> UploadImage()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "/uploads/";
+            // Check if the request contains multipart/form-data.
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+            if(!Directory.Exists(path)) {
+                Directory.CreateDirectory(path);
+            }
+            var streamProvider = new MultipartFormDataStreamProvider(path);
+            var task = await Request.Content.ReadAsMultipartAsync(streamProvider);
+
+            var img = CopyToDarknet(streamProvider.FileData[0].LocalFileName);
+
+            File.Delete(streamProvider.FileData[0].LocalFileName);
+
+            var result = new HttpResponseMessage(HttpStatusCode.OK);
+            result.Content = new StringContent(RunImageRecognition(img[0], img[1]));
+            return result;
+        }
+
+        public static string Run(string fileName, string args)
+        {
+            string returnvalue = "";
+
+            ProcessStartInfo info = new ProcessStartInfo(fileName);
+            info.WorkingDirectory = WORKING_DIR;
+            info.UseShellExecute = false;
+            info.Arguments = args;
+            info.RedirectStandardInput = true;
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardError = true;
+            info.CreateNoWindow = true;
+
+            using (Process process = Process.Start(info))
+            {
+                returnvalue = process.StandardOutput.ReadToEnd();
+            }
+
+            return returnvalue += "\r\nDone.";
+        }
+
+        public string RunImageRecognition(string imgPath, string imgAbsPath)
+        {
+            var result = Run(WORKING_DIR + "\\darknet_no_gpu.exe",
+                "detector test data/coco.data cfg/yolo.cfg yolo.weights " + imgPath);
+            //clean up upload
+            File.Delete(imgAbsPath);
+            //get image
+
+            return result;
+        }
+
+        public HttpResponseMessage HandleError(string e) {
+            var err = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            err.ReasonPhrase = e;
+            return err;
+        }
+
+        public string[] CopyToDarknet(string startPath) {
+            File.Copy(startPath, WORKING_DIR + "\\data\\upload.jpg");
+            return new string[] { "data/upload.jpg", WORKING_DIR + "\\data\\upload.jpg" };
+        }
+    }
+}
